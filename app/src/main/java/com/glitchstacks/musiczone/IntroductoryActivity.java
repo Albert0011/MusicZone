3ec6175322a98556b76d6ffa3d176ff21ba5e692
Albert0011 <64696930+Albert0011@users.noreply.github.com>
2020-12-03 19:37:29 +0700

    tutor part 2

--- app/src/main/java/com/glitchstacks/musiczone/IntroductoryActivity.java
+++ app/src/main/java/com/glitchstacks/musiczone/IntroductoryActivity.java
@@ -1,14 +1,69 @@
 package com.glitchstacks.musiczone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+
 
 public class IntroductoryActivity extends AppCompatActivity {
+
+    ImageView logo;
+    LottieAnimationView lottieAnimationView;
+
+    private static final int NUM_PAGES = 3;
+    private ViewPager viewPager;
+    private ScreenSlidePagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introductory);
+
+        logo = findViewById(R.id.logo);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        viewPager = findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        logo.animate().translationY(-2000).setDuration(1000).setStartDelay(3000);
+        lottieAnimationView.animate().translationY(2000).setDuration(1000).setStartDelay(3000);
+
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position){
+                case 0:
+                    OnBoardingFragment1 tab1 = new OnBoardingFragment1();
+                    return tab1;
+                case 1:
+                    OnBoardingFragment2 tab2 = new OnBoardingFragment2();
+                    return tab2;
+                case 2:
+                    OnBoardingFragment3 tab3 = new OnBoardingFragment3();
+                    return tab3;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
