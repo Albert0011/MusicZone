7a22e0e4ec5a3180b26e8de41d4b1aff9aed20cd
Albert0011 <64696930+Albert0011@users.noreply.github.com>
2020-12-06 03:51:13 +0700

    validasi kelar

--- app/src/main/java/com/glitchstacks/musiczone/Common/SignUp.java
+++ app/src/main/java/com/glitchstacks/musiczone/Common/SignUp.java
@@ -6,6 +6,7 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.glitchstacks.musiczone.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignUp extends AppCompatActivity {
 
@@ -20,6 +22,8 @@ public class SignUp extends AppCompatActivity {
     ImageView backBtn;
     Button next, signin;
     TextView titletext;
+
+    TextInputLayout fullname, username, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +36,18 @@ public class SignUp extends AppCompatActivity {
         signin = findViewById(R.id.signup_signin_btn);
         titletext = findViewById(R.id.signup_title_text);
 
+        fullname = findViewById(R.id.fullname_input);
+        email = findViewById(R.id.email_input);
+        username = findViewById(R.id.username_input);
+        password = findViewById(R.id.password_input);
 
     }
 
     public void callNextSignUpScreen(View view) {
+
+        if(!validateFullname() | !validateEmail() | !validateUsername() | validatePassword()){
+            return;
+        }
 
         Intent intent = new Intent(getApplicationContext(), SignUp2ndClass.class);
 
@@ -55,24 +67,102 @@ public class SignUp extends AppCompatActivity {
         }
     }
 
-    public void backToBefore(View view){
+    //Validation
+    private boolean validateFullname() {
+        String val = fullname.getEditText().getText().toString().trim();
+
+        if (val.isEmpty()) {
+            fullname.setError("Field cannot be empty");
+            return false;
+        } else {
+            fullname.setError(null);
+            fullname.setErrorEnabled(false);
+            return true;
+        }
+
+    }
+
+    private boolean validateUsername() {
+        String val = username.getEditText().getText().toString().trim();
+        String checkspaces = "\\A\\w(1,20)\\z";
+
+        if (val.isEmpty()) {
+            fullname.setError("Field cannot be empty");
+            return false;
+        } else if (val.length() > 20) {
+            username.setError("Username is too long!");
+            return false;
+        } else if (!val.matches(checkspaces)) {
+            username.setError("White space is now allowed!");
+            return false;
+        } else {
+            fullname.setError(null);
+            fullname.setErrorEnabled(false);
+            return true;
+        }
+
+    }
+
+    private boolean validateEmail() {
+        String val = email.getEditText().getText().toString().trim();
+        String checkemail = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (val.isEmpty()) {
+            fullname.setError("Field cannot be empty");
+            return false;
+        } else if (!val.matches(checkemail)) {
+            username.setError("Invalid Email Address");
+            return false;
+        } else {
+            fullname.setError(null);
+            fullname.setErrorEnabled(false);
+            return true;
+        }
+
+    }
+
+    private boolean validatePassword() {
+        String val = password.getEditText().getText().toString().trim();
+        String checkPassword = "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                //"(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+
+        if (val.isEmpty()) {
+            password.setError("Field can not be empty");
+            return false;
+        } else if (!val.matches(checkPassword)) {
+            password.setError("Password should contain 4 characters!");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public void backToBefore(View view) {
 
         Intent intent = new Intent(getApplicationContext(), MusicZoneStartUpScreen.class);
 
         Pair[] pairs = new Pair[1];
 
-        pairs[0] = new Pair<View, String>(findViewById(R.id.signup_back_btn),"transition_back_btn");
+        pairs[0] = new Pair<View, String>(findViewById(R.id.signup_back_btn), "transition_back_btn");
 
-        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SignUp.this, pairs);
-            startActivity(intent,options.toBundle());
-        }
-        else{
+            startActivity(intent, options.toBundle());
+        } else {
             startActivity(intent);
         }
     }
 
-    public void callLoginScreen(View view){
+    public void callLoginScreen(View view) {
 
         Intent intent = new Intent(getApplicationContext(), Login.class);
         startActivity(intent);
--- app/src/main/java/com/glitchstacks/musiczone/Common/SignUp2ndClass.java
+++ app/src/main/java/com/glitchstacks/musiczone/Common/SignUp2ndClass.java
@@ -9,10 +9,16 @@ import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.glitchstacks.musiczone.R;
+
+import java.util.Calendar;
 
 public class SignUp2ndClass extends AppCompatActivity {
 
@@ -20,6 +26,10 @@ public class SignUp2ndClass extends AppCompatActivity {
     ImageView backBtn;
     Button next, signin;
     TextView titletext;
+
+    RadioGroup radioGroup;
+    RadioButton selectedGender;
+    DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +41,26 @@ public class SignUp2ndClass extends AppCompatActivity {
         next = findViewById(R.id.signup_next_btn);
         signin = findViewById(R.id.signup_signin_btn);
         titletext = findViewById(R.id.signup_title_text);
+        radioGroup = findViewById(R.id.radio_group);
+        datePicker = findViewById(R.id.age_picker);
 
 
     }
 
     public void callNextSignUpScreen(View view) {
+
+        if(!validateGender() | !validateAge()){
+            return;
+        }
+
+        selectedGender =  findViewById(radioGroup.getCheckedRadioButtonId());
+        String _gender = selectedGender.getText().toString();
+
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year = datePicker.getYear();
+
+        String date = day+"/"+month+"/"+year;
 
         Intent intent = new Intent(getApplicationContext(), SignUpActivity3rdClass.class);
 
@@ -55,6 +80,29 @@ public class SignUp2ndClass extends AppCompatActivity {
         }
 
     }
+
+    private boolean validateGender() {
+        if (radioGroup.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(this, "Please Select Gender", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    private boolean validateAge() {
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int userAge = datePicker.getYear();
+        int isAgeValid = currentYear - userAge;
+
+        if (isAgeValid < 14) {
+            Toast.makeText(this, "You are not eligible to apply", Toast.LENGTH_SHORT).show();
+            return false;
+        } else
+            return true;
+    }
+
 
     public void backToBefore(View view){
 
