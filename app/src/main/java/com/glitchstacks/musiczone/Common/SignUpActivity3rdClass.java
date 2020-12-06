310a9334133a4adeab8dfdbaa33e45f30d9dd2e3
Albert0011 <64696930+Albert0011@users.noreply.github.com>
2020-12-06 11:08:58 +0700

    ndak tau gua menyerah

--- app/src/main/java/com/glitchstacks/musiczone/Common/SignUpActivity3rdClass.java
+++ app/src/main/java/com/glitchstacks/musiczone/Common/SignUpActivity3rdClass.java
@@ -8,15 +8,35 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ScrollView;
 
 import com.glitchstacks.musiczone.R;
+import com.google.android.material.textfield.TextInputLayout;
+import com.hbb20.CountryCodePicker;
 
 public class SignUpActivity3rdClass extends AppCompatActivity {
+
+    //Variables
+    ScrollView scrollView;
+    TextInputLayout phoneNumber;
+    CountryCodePicker countryCodePicker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up_activity3rd_class);
+
+        //Hooks
+
+        scrollView = findViewById(R.id.signup_scrollview);
+        countryCodePicker = findViewById(R.id.signup_phone_number);
+        phoneNumber = findViewById(R.id.signup_phone_number);
+
+
     }
 
     public void backToBefore(View view){
@@ -36,7 +56,63 @@ public class SignUpActivity3rdClass extends AppCompatActivity {
         }
     }
 
-    public void
+    public void callVerifyOTPScreen(View view){
+
+        if(!validatePhoneNumber()){
+            return;
+        }
+
+        //Get values from previous screen
+
+
+        String _fullname = getIntent().getStringExtra("fullname");
+        String _email = getIntent().getStringExtra("email");
+        String _username = getIntent().getStringExtra("username");
+        String _password = getIntent().getStringExtra("password");
+        String _date = getIntent().getStringExtra("date");
+        String _gender = getIntent().getStringExtra("gender");
+
+        String _getUserEnteredPhoneNumber = phoneNumber.getEditText().getText().toString().trim();
+        String _phoneNo = "+"+countryCodePicker.getFullNumber()+_getUserEnteredPhoneNumber;
+
+
+        Intent intent = new Intent(getApplicationContext(), VerifyOTP.class);
+
+        //Pass all fields to the next activity
+        intent.putExtra("fullName", _fullname);
+        intent.putExtra("email", _email);
+        intent.putExtra("username", _username);
+        intent.putExtra("password", _password);
+        intent.putExtra("date", _date);
+        intent.putExtra("gender", _gender);
+        intent.putExtra("phoneNo", _phoneNo);
+        intent.putExtra("whatToDO", "createNewUser"); // This is to identify that which action should OTP perform after verification.
+
+        //Add Transition
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View, String>(scrollView, "transition_OTP_screen");
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SignUpActivity3rdClass.this, pairs);
+            startActivity(intent, options.toBundle());
+        } else {
+            startActivity(intent);
+        }
+
+    }
+
+
+    //ini gua buat sendiri di tutor g ada
+    private boolean validatePhoneNumber(){
+
+        String val = phoneNumber.getEditText().getText().toString().trim();
+
+        if (val.isEmpty()) {
+            phoneNumber.setError("Field cannot be empty");
+            return false;
+        }
+
+
+    }
 
     public void callLoginScreen(View view){
 
--- app/src/main/java/com/glitchstacks/musiczone/Common/VerifyOTP.java
+++ app/src/main/java/com/glitchstacks/musiczone/Common/VerifyOTP.java
@@ -3,14 +3,38 @@ package com.glitchstacks.musiczone.Common;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
+import com.chaos.view.PinView;
 import com.glitchstacks.musiczone.R;
 
 public class VerifyOTP extends AppCompatActivity {
 
+
+    PinView pinFromUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_verify_o_t_p);
+
+        //hooks
+        pinFromUser = findViewById(R.id.pin_view);
+        String _phoneNo = getIntent().getStringExtra("phoneNo");
+        sendVerificationCodeToUser(_phoneNo);
+
+    }
+
+    private void sendVerificationCodeToUser(String phoneNo) {
+
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+
     }
 }
